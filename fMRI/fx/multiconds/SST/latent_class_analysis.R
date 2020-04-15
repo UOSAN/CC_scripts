12666db73f8ca7dbd4eea45d56e30756e1130167
Paul Novak <pnovak@alumni.caltech.edu>
2020-04-15 10:54:32 -0700

    Print the classifications of each subject

--- fMRI/fx/multiconds/SST/latent_class_analysis.R
+++ fMRI/fx/multiconds/SST/latent_class_analysis.R
@@ -5,8 +5,8 @@ library(dplyr)
 input_dir=""
 filenames = dir(input_dir, pattern="latent_class_analysis.tsv", full.names = TRUE)
 
-dataset = read.table(filenames[1], sep="\t", header = TRUE)
-dataset = dplyr::select(dataset, failed_go, mean_rt, std_dev_rt)
+temp = read.table(filenames[1], sep="\t", header = TRUE)
+dataset = dplyr::select(temp, failed_go, mean_rt, std_dev_rt)
 
 BIC = mclustBIC(dataset)
 plot(BIC)
@@ -19,13 +19,11 @@ summary(ICL)
 model_VEI4 = Mclust(dataset, modelNames = 'VEI', G = 4, x = BIC)
 plot(model_VEI4, what = 'classification')
 
+cbind(select(temp, subject_id), model_VEI4$classification)
+
 model_VVI2 = Mclust(dataset, modelNames = 'VVI', G = 2, x = BIC)
 plot(model_VVI2, what = 'classification')
 
-
-means = data.frame(model_VEI4$parameters$mean, stringsAsFactors = FALSE) %>%
-  tibble::rownames_to_column() %>%
-  rename(manifest = rowname)
 # summary(model_VEI4)
 # 
 # mclustBootstrapLRT(dataset, modelName = 'VEI')
